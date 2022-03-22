9c96cf2c4e66f429ce084a44966936be3adddece
David Velasco <velasco.david@correounivalle.edu.co>
2022-03-22 11:51:49 -0500

    Se agrega el segundo procedimiento

--- Challenge No. 3.sql
+++ Challenge No. 3.sql
@@ -1,6 +1,5 @@
+--PROCEDIMIENTO 1
 --procedimiento, al ser llamado con un id de una prueba, actualiza la nota total de la prueba segun las preguntas y respuestas del estudiante en esa prueba
-
-
 
 CREATE OR REPLACE PROCEDURE calificar_prueba(Prueba_idUwU INT)
     AS $calificar_prueba$
@@ -22,9 +21,47 @@ CREATE OR REPLACE PROCEDURE calificar_prueba(Prueba_idUwU INT)
     END;
 $calificar_prueba$ LANGUAGE plpgsql;
 
--- Para correr los procedimientos se debe ejecutar la view primero
+-- Para Poder visualizar este procedimento, se requiere correr la vista primero
 --SELECT * FROM calificaciones_estudiante;
 
---call calificar_prueba (1);
+call calificar_prueba (1);
 
 --SELECT * FROM calificaciones_estudiante;
+
+--PROCEDIMIENTO 2
+--un procedimiento que reciba un administrador_id y actualize el regitro de ese adeministrador con una contrasenha mas segura
+CREATE OR REPLACE PROCEDURE crear_contrasenha(Administrador_idUwU INT)
+    AS $crear_contrasenha$
+    DECLARE
+    	char1 CHAR = '';
+	 	temp_sum CHAR = '';
+    	charI INT  = 0;
+    	password1 VARCHAR(100) = '';
+    	len INT = 12;
+    BEGIN
+        IF EXISTS(SELECT * FROM Administrador AS A WHERE A.Administrador_id = Administrador_idUwU) THEN
+            BEGIN            
+                WHILE len > 0 LOOP
+                    BEGIN
+                    	charI = floor(random() * 100 + 1)::int;
+        				IF charI > 48 AND charI < 122 THEN
+        					BEGIN
+          						password1 := CONCAT(password1,CHR(charI));
+          						len := len - 1;
+                    		END;
+                    	END IF;
+                   	END;
+                END LOOP;
+                UPDATE Administrador AS A
+                SET Contrasenha = password1
+                WHERE A.Administrador_id = Administrador_idUwU;
+            END;
+        ELSE
+            RAISE EXCEPTION 'No existe el administrador con ese id';        
+        END IF;
+    END;
+$crear_contrasenha$ LANGUAGE plpgsql;
+
+SELECT * FROM administrador;
+CALL crear_contrasenha(1);
+SELECT * FROM administrador;
